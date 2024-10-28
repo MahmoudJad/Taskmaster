4a090e5e1691bcea0c332eb6175821f8302bdb8c
MahmoudJad <mr-jako@hotmail.com>
2024-10-28 12:41:01 +0300

    add prod db

--- database.py
+++ database.py
@@ -3,21 +3,21 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 import constant
 
-# SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:admin@12345@localhost/Todo'
+SQLALCHEMY_DATABASE_URL = 'postgresql://taskmaster_db_owner:RwWlB9bscgX0@ep-proud-hall-a55sfpxz.us-east-2.aws.neon.tech/taskmaster_db?sslmode=require'
 
 
-DATABASE_URL = (
-    "postgresql://"
-    + constant.POSTGRES_USER
-    + ":"
-    + constant.POSTGRES_PASSWORD
-    + "@"
-    + constant.POSTGRES_SERVER
-    + "/"
-    + constant.POSTGRES_DB
-)
+# DATABASE_URL = (
+#     "postgresql://"
+#     + constant.POSTGRES_USER
+#     + ":"
+#     + constant.POSTGRES_PASSWORD
+#     + "@"
+#     + constant.POSTGRES_SERVER
+#     + "/"
+#     + constant.POSTGRES_DB
+# )
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
